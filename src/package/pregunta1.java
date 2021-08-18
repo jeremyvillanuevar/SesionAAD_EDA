c25a599200cb33a5d459af0df657dc2bced95699
jeremyvillanuevar <78555867+jeremyvillanuevar@users.noreply.github.com>
2021-08-17 21:40:40 -0500

    Update pregunta1.java

--- src/package/pregunta1.java
+++ src/package/pregunta1.java
@@ -1,5 +1,6 @@
-
+//Esto para el random
 import java.lang.Math;
+//Fin del la librería para el random.
 public class pregunta1{
     public static void main(String[] args)
     {
@@ -7,15 +8,21 @@ public class pregunta1{
         int numero;
         int digito;
         int codigo;
-        
+
+        //Esto para el random
         int max = 9;
         int min = 1;
         int range = max - min + 1;
-          
+        //Fin del la librería para el random.
+
         //  Leemos el valor del número
         numero = 28985;
         //  Calcular Dígito: Aleatorio
+
+        //Esto para el random
         digito=  (int)(Math.random() * range) + min;
+        //Fin del la librería para el random.
+
         //  Calcular Código: Código = Número * 10 + Dígito
         codigo=numero*10+digito;
         //  Mostrar mensaje el "Código es"
