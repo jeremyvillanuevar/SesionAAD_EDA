547be2ba918cbc2189a739e0830af44d9fe6282a
jeremyvillanuevar <78555867+jeremyvillanuevar@users.noreply.github.com>
2021-08-17 21:25:54 -0500

    Update pseudocodigo.java

--- src/package/pseudocodigo.java
+++ src/package/pseudocodigo.java
@@ -3,17 +3,19 @@ public class pseudocodigo{
     public static void main(String[] args)
     {
         // Inicio
-        int area;
-        int lado;
+        double areav2;
+        int ladox;
         //     Leemos el valor de un lado del cuadrado
-        lado=9;
+        ladox=3;
         //     Calcular Area del Cuadrado: Area = lado * lado
-        area = lado*lado;
+        areav2 = ladox*ladox;
         //     Mostrar mensaje el "Area es"
         System.out.println("Área es igual a: ");
         //     Mostrar el valor de Area
-        System.out.println(area);
+        System.out.println(areav2);
         // Fin
-        // Holas!!
+        System.out.println("Jeremy estuvo aquí");
+        System.out.println("System.out.println sirve para enviar mensajes");
+
     }
 }
